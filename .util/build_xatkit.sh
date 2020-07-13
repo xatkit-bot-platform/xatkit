8b8b7c60e9e62570334ce4bd4070bb646fd41cb7
Hamza <hamza.eddouibi@gmail.com>
2020-07-14 00:09:49 +0200

    Fix code indentation

--- .util/build_xatkit.sh
+++ .util/build_xatkit.sh
@@ -2,15 +2,15 @@
 
 # Print a message
 e() {
-    echo -e "$1"
+  echo -e "$1"
 }
 
 main() {
-	e "Building Xatkit"
-    e "Please wait..."
-    git submodule update --init --recursive  > /dev/null
-    ./build.sh --all --skip-tests --product  > /dev/null
-    e "Finished building Xatkit"
+  e "Building Xatkit"
+  e "Please wait..."
+  git submodule update --init --recursive  > /dev/null
+  ./build.sh --all --skip-tests --product  > /dev/null
+  e "Finished building Xatkit"
 }
 
 main
--- .util/start_xatkit.sh
+++ .util/start_xatkit.sh
@@ -1,14 +1,14 @@
 # Start GreetingsBot
 # Print a message
 e() {
-    echo -e "$1"
+  echo -e "$1"
 }
 
 main() {
 	e "Deploying GreetingsBot"
-    cd $XATKIT/bin
-    chmod +x start-xatkit-linux.sh
-    ./start-xatkit-linux.sh ../examples/GreetingsBots/GreetingsBot/GreetingsBot.properties  > /dev/null &
+	cd $XATKIT/bin
+	chmod +x start-xatkit-linux.sh
+	./start-xatkit-linux.sh ../examples/GreetingsBots/GreetingsBot/GreetingsBot.properties  > /dev/null &
 }
 
 main
